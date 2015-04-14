87adf249f5b73bd56d1ab0439e6d13a09a1bd29c
Matthew Davidson <matthew.christopher.davidson@gmail.com>
2015-04-14 14:25:51 -0700

    Update index.js
    Node inherits and Backbone/CoffeeScript extend.

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 (function ( root, factory ) {
-	if( typeof define !== 'undefined' && define.amd ) {
-		define([],factory);
-	} else if (typeof exports === 'object') {
-		module.exports = factory();
-	} else {
-		root.inherits = factory();
-	}
+    if( typeof define !== 'undefined' && define.amd ) {
+        define([],factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.inherits = factory();
+    }
 })(this, function (){
 
-	var hasOwn = {}.hasOwnProperty;
-
-	return function inherits(child, parent) {
-		for (var key in parent) {
-			if (hasOwn.call(parent, key)) child[key] = parent[key];
-		}
-		function ctor() {
-			this.constructor = child;
-		}
-		ctor.prototype = parent.prototype;
-		child.prototype = new ctor();
-		//CoffeeScript API
-		child.__super__ = parent.prototype;
-		//node API
-		child.super_ = parent;
-		return child;
-	};
-
+    var hasOwn = {}.hasOwnProperty;
+    return (typeof Object.create == 'function' ) ? function ( child, parent ) {
+            //Backbone/CoffeeScript API
+            for (var key in parent) {
+                if (hasOwn.call(parent, key)) child[key] = parent[key];
+            }
+            child.__super__ = parent.prototype;
+            //node API
+            child.super_ = parent;
+            child.prototype = Object.create(parent.prototype, {
+                constructor: {
+                    value: child,
+                    enumerable: false,
+                    writable: true,
+                    configurable: true
+                }
+            })
+        }
+    : function (child, parent) {
+        for (var key in parent) {
+            if (hasOwn.call(parent, key)) child[key] = parent[key];
+        }
+        function ctor() {
+            this.constructor = child;
+        }
+        ctor.prototype = parent.prototype;
+        child.prototype = new ctor();
+        //CoffeeScript API
+        child.__super__ = parent.prototype;
+        //node API
+        child.super_ = parent;
+        return child;
+    };
 });
 
